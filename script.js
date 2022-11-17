5edf6c83ceb5328ba0b931e7bbf9d4ba767c6b34
paulcastr0 <castropaul.learn@gmail.com>
2022-11-17 15:48:54 +0100

    Completamento Milestone 0 & 1

---
+++ script.js
@@ -0,0 +1,53 @@
+/*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede (BOOTSTRAP!)
+Consigli del giorno:
+Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+*/
+
+//Dichiaro Array membri del team
+let ourMembers = [
+    {
+        name : "Wayne Barnett",
+        role : "Founder & CEO",
+        picture : 123
+    },
+    {
+        name : "Angela Caroll",
+        role : "Chief Editor",
+        picture : 123
+    },
+    {
+        name : "Walter Gordon",
+        role : "Office Manager",
+        picture : 123
+    },
+    {
+        name : "Angela Lopez",
+        role : "social Media Manager",
+        picture : 123
+    },
+    {
+        name : "Scott Estrada",
+        role : "Developer",
+        picture : 123
+    },
+    {
+        name : "Barbara Ramos",
+        role : "graphic Designer",
+        picture : 123
+    }
+]
+console.log(ourMembers);
+
+for ()
